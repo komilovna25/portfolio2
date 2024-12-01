aa5d51f2a9168d35ed4ee6e5455543426a67ecdd
Ocak <sayfullayeva191@gmail.com>
2024-12-01 16:10:53 +0500

    portfolio

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,5 @@
 import requests
 
-# Telegram bot configuration
 from config.settings import CHAT_ID, BOT_TOKEN
 
 def send_message(text):
@@ -11,13 +10,11 @@ def send_message(text):
     CHAT_IDs = CHAT_ID
     PHOTO = "https://cdn.create.vista.com/api/media/small/223453354/stock-photo-cropped-shot-businesspeople-working-workplace-digital-devices-documents"
     
-    # The URL to send the message
     url = f"https://api.telegram.org/bot{BOT_TOKENs}/sendphoto?chat_id={CHAT_IDs}&photo={PHOTO}&caption={text}"
 
-    # Send the message using a GET request (you can use POST as well if you prefer)
     response = requests.get(url)
 
-    # Check if the request was successful
+ 
     if response.status_code == 200:
         print("Message sent successfully to Telegram bot!")
     else:
